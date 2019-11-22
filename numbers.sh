04133602cb61835a69ada49f8e17e6b0497315a5
Max Pugh <maxpugh02@gmail.com>
2019-11-22 12:28:32 -0800

    fixed infinite loop

--- numbers.sh
+++ numbers.sh
@@ -3,13 +3,13 @@
 echo "Enter a positive integer: "
 read n
 i=1
-while [ $i -le "$n" ]
+while [ i -le "$n" ]
 do
 if [ $(( $n % 2 )) -eq 0 ];then
 	echo "$n is even number"
 else
 	echo "$n is odd number"
 	fi
-n=$((n+1))
+n=$((i+1))
 done
 
